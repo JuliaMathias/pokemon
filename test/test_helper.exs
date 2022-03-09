7653e624062cdc159c8369d7401bfef06ec1d52b
JuliaMathias <juliamss@gmail.com>
2022-03-09 15:11:05 -0300

    chore: configure mox for tests

--- test/test_helper.exs
+++ test/test_helper.exs
@@ -1,2 +1,7 @@
 ExUnit.start()
+
+# Configure Mox
+Mox.defmock(BehaviourMock, for: Pokemon.Http.Behaviour)
+Application.put_env(:pokemon, Pokemon.Pokemons.PokemonRepository, BehaviourMock)
+
 Ecto.Adapters.SQL.Sandbox.mode(Pokemon.Repo, :manual)
