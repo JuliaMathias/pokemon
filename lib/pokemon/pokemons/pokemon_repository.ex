a7c04a37b2517f45aac5ef85ea1459669c278578
JuliaMathias <juliamss@gmail.com>
2022-03-09 14:29:43 -0300

    feat: get_name_by_id/1 and get_id_by_name/1

---
+++ lib/pokemon/pokemons/pokemon_repository.ex
@@ -0,0 +1,23 @@
+defmodule Pokemon.Pokemons.PokemonRepository do
+  @moduledoc """
+  Functions that deal with our lovely Pokemons
+  """
+
+  @spec get_name_by_id(id :: String.t()) :: String.t() | {:error, atom}
+  def get_name_by_id(id) do
+    case impl().get_by_id(id) do
+      {:ok, %{"name" => name}} -> name
+      error -> error
+    end
+  end
+
+  @spec get_id_by_name(name :: String.t()) :: String.t() | {:error, atom}
+  def get_id_by_name(name) do
+    case impl().get_by_name(name) do
+      {:ok, %{"id" => id}} -> id
+      error -> error
+    end
+  end
+
+  defp impl, do: Application.get_env(:pokemon, __MODULE__, Pokemon.Http.Client)
+end
