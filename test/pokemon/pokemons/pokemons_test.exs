f7cb81b86ced57a2f77f81390e9c23fd5b7efc02
JuliaMathias <juliamss@gmail.com>
2022-03-09 20:33:09 -0300

    chore: rename modules

--- test/pokemon/pokemons/pokemons_test.exs
+++ test/pokemon/pokemons/pokemons_test.exs
@@ -1,44 +1,44 @@
-defmodule Pokemon.Pokemons.PokemonRepositoryTest do
+defmodule Pokemon.Pokemons.PokemonsTest do
   @moduledoc false
   use ExUnit.Case, async: true
 
   import Mox
 
-  alias Pokemon.Pokemons.PokemonRepository
+  alias Pokemon.Pokemons.Pokemons
 
   setup :verify_on_exit!
 
   test "gets name of a pokemon when given an id" do
     expect(PokeApiBehaviourMock, :get_by_id, fn _id -> {:ok, %{"name" => "ditto"}} end)
 
-    assert PokemonRepository.get_name_by_id("132") == "ditto"
+    assert Pokemons.get_name_by_id("132") == "ditto"
   end
 
   test "gets id of a pokemon when given a name" do
     expect(PokeApiBehaviourMock, :get_by_name, fn _name -> {:ok, %{"id" => 132}} end)
 
-    assert PokemonRepository.get_id_by_name("ditto") == 132
+    assert Pokemons.get_id_by_name("ditto") == 132
   end
 
   test "when id is given and the pokemon is not found return {:error, :not_found}" do
     expect(PokeApiBehaviourMock, :get_by_id, fn _id -> {:error, :not_found} end)
 
-    assert PokemonRepository.get_name_by_id("9999999") == {:error, :not_found}
+    assert Pokemons.get_name_by_id("9999999") == {:error, :not_found}
   end
 
   test "when name is given and the pokemon is not found return {:error, :not_found}" do
     expect(PokeApiBehaviourMock, :get_by_name, fn _name -> {:error, :not_found} end)
 
-    assert PokemonRepository.get_id_by_name("julia") == {:error, :not_found}
+    assert Pokemons.get_id_by_name("julia") == {:error, :not_found}
   end
 
   test "if api is unavailable should return {:error, :unexpected}" do
     expect(PokeApiBehaviourMock, :get_by_id, fn _id -> {:error, :unexpected} end)
 
-    assert PokemonRepository.get_name_by_id("132") == {:error, :unexpected}
+    assert Pokemons.get_name_by_id("132") == {:error, :unexpected}
 
     expect(PokeApiBehaviourMock, :get_by_name, fn _name -> {:error, :unexpected} end)
 
-    assert PokemonRepository.get_id_by_name("ditto") == {:error, :unexpected}
+    assert Pokemons.get_id_by_name("ditto") == {:error, :unexpected}
   end
 end
--- test/test_helper.exs
+++ test/test_helper.exs
@@ -2,6 +2,6 @@ ExUnit.start()
 
 # Configure Mox
 Mox.defmock(PokeApiBehaviourMock, for: Pokemon.PokeApi.Behaviour)
-Application.put_env(:pokemon, Pokemon.Pokemons.PokemonRepository, PokeApiBehaviourMock)
+Application.put_env(:pokemon, Pokemon.Pokemons.Pokemons, PokeApiBehaviourMock)
 
 Ecto.Adapters.SQL.Sandbox.mode(Pokemon.Repo, :manual)
