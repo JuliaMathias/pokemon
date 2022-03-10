b7de1c909552e46c35fc3fc377287a5f60992c2d
JuliaMathias <juliamss@gmail.com>
2022-03-09 22:10:24 -0300

    test: search the database before making a request

--- test/pokemon/pokemons/pokemons_test.exs
+++ test/pokemon/pokemons/pokemons_test.exs
@@ -2,22 +2,74 @@ defmodule Pokemon.Pokemons.PokemonsTest do
   @moduledoc false
   use ExUnit.Case, async: true
 
+  import Ecto.Query
   import Mox
 
   alias Pokemon.Pokemons.Pokemons
+  alias Pokemon.Pokemons.PokemonSchema
+  alias Pokemon.Repo
 
   setup :verify_on_exit!
 
-  test "gets name of a pokemon when given an id" do
-    expect(PokeApiBehaviourMock, :get_by_id, fn _id -> {:ok, %{"name" => "ditto"}} end)
-
-    assert Pokemons.get_name_by_id("132") == "ditto"
+  setup do
+    :ok = Ecto.Adapters.SQL.Sandbox.checkout(Repo)
   end
 
-  test "gets id of a pokemon when given a name" do
-    expect(PokeApiBehaviourMock, :get_by_name, fn _name -> {:ok, %{"id" => 132}} end)
+  describe "using the database" do
+    setup do
+      id = "8"
+      name = "wartortle"
 
-    assert Pokemons.get_id_by_name("ditto") == 132
+      Repo.insert(%PokemonSchema{
+        pokeapi_id: id,
+        name: name,
+        response: %{"name" => name, "id" => 8}
+      })
+
+      {:ok, %{id: id, name: name}}
+    end
+
+    test "gets name of a pokemon when given an id", %{id: id, name: name} do
+      assert Pokemons.get_name_by_id(id) == name
+    end
+
+    test "gets id of a pokemon when given a name", %{id: id, name: name} do
+      assert Pokemons.get_id_by_name(name) == id
+    end
+  end
+
+  describe "using the api" do
+    test "gets name of a pokemon when given an id and saves to database" do
+      id = "132"
+      name = "ditto"
+      response = %{"name" => name, "id" => 132}
+
+      expect(PokeApiBehaviourMock, :get_by_id, fn _id -> {:ok, response} end)
+
+      assert Pokemons.get_name_by_id(id) == name
+
+      pokemon = Repo.one(from p in PokemonSchema, where: p.pokeapi_id == ^id)
+
+      assert pokemon.pokeapi_id == id
+      assert pokemon.name == name
+      assert pokemon.response == response
+    end
+
+    test "gets id of a pokemon when given a name and saves to database" do
+      id = "15"
+      name = "beedrill"
+      response = %{"name" => name, "id" => 15}
+
+      expect(PokeApiBehaviourMock, :get_by_name, fn _name -> {:ok, response} end)
+
+      assert Pokemons.get_id_by_name(name) == id
+
+      pokemon = Repo.one(from p in PokemonSchema, where: p.name == ^name)
+
+      assert pokemon.pokeapi_id == id
+      assert pokemon.name == name
+      assert pokemon.response == response
+    end
   end
 
   test "when id is given and the pokemon is not found return {:error, :not_found}" do
