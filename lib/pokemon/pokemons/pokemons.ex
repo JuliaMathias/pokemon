e7a1c94281f15e62802388cba4c6e029d46303fd
JuliaMathias <juliamss@gmail.com>
2022-03-09 22:10:00 -0300

    feat: search the database before making a request

--- lib/pokemon/pokemons/pokemons.ex
+++ lib/pokemon/pokemons/pokemons.ex
@@ -3,20 +3,37 @@ defmodule Pokemon.Pokemons.Pokemons do
   Functions that deal with our lovely Pokemons
   """
 
+  import Ecto.Query
+
+  alias Pokemon.Pokemons.PokemonSchema
+  alias Pokemon.Repo
+
   @spec get_name_by_id(id :: String.t()) :: String.t() | {:error, atom}
   def get_name_by_id(id) do
-    case impl().get_by_id(id) do
-      {:ok, %{"name" => name}} -> name
+    with nil <- Repo.one(from p in PokemonSchema, where: p.pokeapi_id == ^id),
+         {:ok, pokemon} <- impl().get_by_id(id),
+         {:ok, %PokemonSchema{name: name}} <- save_pokemon(pokemon) do
+      name
+    else
+      %PokemonSchema{name: name} -> name
       error -> error
     end
   end
 
-  @spec get_id_by_name(name :: String.t()) :: integer | {:error, atom}
+  @spec get_id_by_name(name :: String.t()) :: String.t() | {:error, atom}
   def get_id_by_name(name) do
-    case impl().get_by_name(name) do
-      {:ok, %{"id" => id}} -> id
+    with nil <- Repo.one(from p in PokemonSchema, where: p.name == ^name),
+         {:ok, pokemon} <- impl().get_by_name(name),
+         {:ok, %PokemonSchema{pokeapi_id: pokeapi_id}} <- save_pokemon(pokemon) do
+      pokeapi_id
+    else
+      %PokemonSchema{pokeapi_id: id} -> id
       error -> error
     end
+  end
+
+  defp save_pokemon(%{"name" => name, "id" => id} = pokemon) do
+    Repo.insert(%PokemonSchema{pokeapi_id: Integer.to_string(id), name: name, response: pokemon})
   end
 
   defp impl, do: Application.get_env(:pokemon, __MODULE__, Pokemon.PokeApi.Client)
