70beed6de79bf9aa03241779fd4b911201ae66ce
JuliaMathias <juliamss@gmail.com>
2022-03-09 22:08:56 -0300

    refactor:  add primary key, and change pokeapi_id to string

--- priv/repo/migrations/20220309232059_create_pokemons.exs
+++ priv/repo/migrations/20220309232059_create_pokemons.exs
@@ -2,9 +2,10 @@ defmodule Pokemon.Repo.Migrations.CreatePokemons do
   use Ecto.Migration
 
   def change do
-    create table(:pokemons) do
+    create table(:pokemons, primary_key: false) do
+      add :id, :uuid, primary_key: true
       add :name, :string
-      add :pokeapi_id, :integer
+      add :pokeapi_id, :string
       add :response, :map, default: %{}
 
       timestamps()
